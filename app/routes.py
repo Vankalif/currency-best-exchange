86fb310fba5898808999cc6588730e463797d44e
Nicholas <vankalif@gmail.com>
2019-01-02 12:26:28 +0300

    dev commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
+import json
+
+mock_answer = json.dumps({'USD': 69.52,
+                          'EUR': 79.66,
+                          'GBP': 88.28})
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/rates')
 def index():
-    return "Hi, I'm here!"
+    return mock_answer
