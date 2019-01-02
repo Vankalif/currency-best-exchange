322f85aab0033c8b91cf484e48da7b3c16678457
Nicholas <vankalif@gmail.com>
2019-01-02 15:28:40 +0300

    dev commit)

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from app import db
+from datetime import datetime
+
+
+class Currencies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String(3), index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return f'<Currency {self.code}>'
+
+
+class Rates(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bank_name = db.Column(db.String(64))
+    currency_code = db.Column(db.String, db.ForeignKey('currencies.id'))
+    buy = db.Column(db.Float, nullable=False)
+    sale = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now())
+
+    def __repr__(self):
+        return f'Buy {self.buy}, Sale {self.sale}, Bank {self.bank_name}'
--- config.py
+++ config.py
@@ -2,6 +2,12 @@ import os
 
 
 class Config(object):
-
     """main config obj for app"""
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'M7cwkj?2019'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
